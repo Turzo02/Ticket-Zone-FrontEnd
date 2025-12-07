73a3aae24bf2726ffe46b93f677d64f6c4926e9c
Turzo02 <naimul198650@gmail.com>
2025-12-07 12:33:50 +0600

    Firebase configuration Done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../Pages/Home/Home";
 import RootLayout from "../Layouts/RootLayout";
+
 export const router = createBrowserRouter([
   {
     path: "/",
